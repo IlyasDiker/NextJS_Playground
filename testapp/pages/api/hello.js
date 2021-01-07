4ea0f74102e9bcf84cf9ad4054c699a4489065eb
IlyasDiker <ilyasbenhssine@gmail.com>
2021-01-07 16:09:21 +0100

    add dbconnect to api

--- testapp/pages/api/hello.js
+++ testapp/pages/api/hello.js
@@ -1,6 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from '../../utils/dbConnect';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+
+// export default (req, res) => {
+//   res.statusCode = 200
+//   res.json({ name: 'John Doe' })
+// }
+
+dbConnect();
+
+export default async (req, res) => {
+  res.json({test: 'test'});
 }
