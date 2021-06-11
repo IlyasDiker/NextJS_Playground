5c8cecf7039e7ec01062949c7272920e243db77d
IlyasDiker <ilyasbenhssine@gmail.com>
2021-06-11 22:11:18 +0100

    add pages + header

---
+++ nextjsapp/components/Layout.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Layout.module.css'
+import Nav from './Nav'
+
+const Layout = ({children}) => {
+    return (
+        <>
+            <Nav />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default Layout
